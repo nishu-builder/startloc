d34b55896c034708cb93aa3c4ba47d3b42c01d76
Nishu Singh <nishu.builder@gmail.com>
2024-05-25 19:09:10 -0700

    Update brew package

--- startloc.rb
+++ startloc.rb
@@ -1,8 +1,8 @@
 class Startloc < Formula
   desc "Tool to dynamically set the starting location for terminal sessions"
   homepage "https://github.com/nishu-builder/startloc"
-  url "https://github.com/nishu-builder/startloc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "668cc6d4da96a068520b17a1ef8a0ffb928570084a9591c019e823ef8109bfe3"
+  url "https://github.com/nishu-builder/startloc/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "a28d2f7b27d5e0e2bf4698a2b2c3f66f4ea804e4ce9dcec68450729b8e22b073"
 
   def install
     bin.install "install_startloc.sh" => "startloc"
